8c6cfbcf40275cc7d0a0c6899d2fed927c17967e
Yzoni <yzonix@gmail.com>
2015-06-23 14:54:46 +0200

    zombie

--- zombies/Zombie.java
+++ zombies/Zombie.java
@@ -1,5 +1,4 @@
 package zombies;
-
 import javax.swing.*;
 import java.awt.*;
 
@@ -11,7 +10,53 @@ class Zombie extends Tile {
 		this.color = Color.green;
 		direction = 1;
 	}
-	
-	public void update() {		
+
+	@Override
+	public void update(currentWorld, tempWorld) {
+		walkZombie(tempWorld.grid);
 	}
+
+	private void walkZombie() {
+
+		Tile possibleTile = findNeighbouringTile(this);
+		this.x = possibleTile.x;
+		this.y = possibleTile.y;
+
+	}
+
+	private Tile findNeighbouringTile(Tile currentTile) {
+
+		Tile tileNorth = grid[x][y + 1];
+		Tile tileSouth = grid[x][y - 1];
+		Tile tileEast = grid[x - 1][y];
+		Tile tileWest = grid[x + 1][y];
+
+		int temp = rand.nextInt(4);
+		for(int i=0; i<4; i++) {
+			int dir = (temp + i) % 4;
+			switch (dir) {
+				case NORTH:
+					if ( tileNorth.color == Color.black || tileNorth.color == Color.pink )
+						return tileNorth;
+					break;
+
+				case SOUTH:
+					if ( tileSouth.color == Color.black || tileSouth.color == Color.pink )
+						return tileSouth;
+					break;
+				case EAST:
+					if ( tileEast.color == Color.black || tileEast.color == Color.pink )
+						return tileEast;
+					break;
+				case WEST:
+					if ( tileWest.color == Color.black || tileWest.color == Color.pink )
+						return tileWest;
+					break;
+			}
+
+		}
+
+		return currentTile;
+	}
+
 }
