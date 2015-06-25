a21ec28f2ee1edeaee439b348eb169ef1fdb8253
Yzoni <yzonix@gmail.com>
2015-06-25 15:26:06 +0200

    imporve menu

--- zombies/Menu.java
+++ zombies/Menu.java
@@ -3,6 +3,9 @@ package zombies;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class Menu extends JPanel {
 	public JSlider zombieCount;
@@ -13,23 +16,58 @@ public class Menu extends JPanel {
 	public JButton newBoard;
 
 	public Menu(World world) {
+
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		this.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+		this.add(Box.createRigidArea(new Dimension(0, 15)));
+
+		JLabel zombieCountLabel = new JLabel("Amount of zombies: " + 10);
+		this.add(zombieCountLabel);
+
 		zombieCount = new JSlider(5, 100);
+		zombieCount.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent changeEvent) {
+				zombieCountLabel.setText("Amount of zombies: "
+						+ zombieCount.getValue());
+			}
+		});
 		zombieCount.setLayout(null);
 		zombieCount.setMajorTickSpacing(5);
 		zombieCount.setPaintTicks(true);
 		zombieCount.setSize(200, 200);
 		zombieCount.setVisible(true);
 		zombieCount.setFocusable(false);
+		zombieCount.setMaximumSize(new Dimension(300, 30));
+		zombieCount.setAlignmentX(-100);
 		this.add(zombieCount);
 
+		this.add(Box.createRigidArea(new Dimension(0, 15)));
+
+		JLabel humanCountLabel = new JLabel("Amount of humans: ");
+		this.add(humanCountLabel);
+
 		humanCount = new JSlider(50, 1000);
+		humanCount.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent changeEvent) {
+				humanCountLabel.setText("Amount of humans: "
+						+ humanCount.getValue());
+			}
+		});
+		zombieCount.setName("HUMANSLIDER");
 		humanCount.setLayout(null);
 		humanCount.setMajorTickSpacing(50);
 		humanCount.setPaintTicks(true);
 		humanCount.setSize(200, 200);
 		humanCount.setVisible(true);
 		humanCount.setFocusable(false);
+		humanCount.setMaximumSize(new Dimension(300, 30));
+		humanCount.setAlignmentX(-100);
 		this.add(humanCount);
+
+		this.add(Box.createRigidArea(new Dimension(0, 15)));
 
 		runBehavior = new JCheckBox("Run");
 		killBehavior = new JCheckBox("Kill");
@@ -56,7 +94,8 @@ public class Menu extends JPanel {
 
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.white);
+		g.setColor(new Color(238, 238, 238));
 		g.fillRect(0, 0, getWidth(), getHeight());
 	}
+
 }
